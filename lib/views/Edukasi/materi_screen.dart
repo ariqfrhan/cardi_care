3b7cc3a85683a439095be44ca3f5fc20d5f53bce
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2024-08-10 02:17:59 +0700

    feat: Add video to materi

--- lib/views/Edukasi/materi_screen.dart
+++ lib/views/Edukasi/materi_screen.dart
@@ -2,6 +2,7 @@ import 'package:cardi_care/model/materi_model.dart';
 import 'package:cardi_care/routes.dart';
 import 'package:cardi_care/shared/theme.dart';
 import 'package:cardi_care/views/widgets/buttons.dart';
+import 'package:cardi_care/views/widgets/video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
@@ -60,6 +61,15 @@ class _MateriState extends State<Materi> {
           SingleChildScrollView(
             child: Html(data: materi.materi),
           ),
+          if (materi.uid == 'materi3') ...[
+            const VideoPlayer(
+                videoPath: "assets/edukasi/materi3/diet_garam_dan_cairan.mp4"),
+            const SizedBox(height: 20),
+            const VideoPlayer(videoPath: "assets/edukasi/materi3/napas_dalam.mp4"),
+          ] else if (materi.uid == 'materi4') ...[
+            const VideoPlayer(
+                videoPath: "assets/edukasi/materi4/six_minute_walk.mp4"),
+          ],
           // Tambahkan padding di bawah untuk memberikan ruang pada konten scroll
           const SizedBox(height: 100),
         ],
