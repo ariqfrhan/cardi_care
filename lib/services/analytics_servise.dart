2d7102913caa76791265169ba13d023be6fe13eb
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2024-09-12 19:05:54 +0700

    add: analytic_service

---
+++ lib/services/analytics_servise.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+class AnalyticsService {
+  final _instance = FirebaseAnalytics.instance;
+
+  Future<void> logMateri(String? userId, String materiId) async {
+    await _instance.logEvent(
+        name: 'materi_pasien',
+        parameters: {"userId": userId, "MateriId": materiId});
+  }
+}
